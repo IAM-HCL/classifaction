7185c6db2c86dba7c577a9706ed7ce705b62ab2d
ly_ys <349152133@qq.com>
2019-12-30 13:41:46 +0800

    1  hidden layer, no wv model

---
+++ procesData.py
@@ -0,0 +1,122 @@
+import numpy as np
+
+reviews = ''
+train_rev = ''
+dev_rev = ''
+test_rev = ''
+
+train_labes = ''
+dev_labes = ''
+test_labes = ''
+
+##1 数据处理
+lineCNT = 0
+with open("./data/train.txt",'r',encoding='utf-8') as rf:
+    for line in rf:
+        if line != '':
+            lineCNT = lineCNT + 1
+            if lineCNT % 2 == 1:
+                train_rev = train_rev + line
+            else:            
+                train_labes = train_labes + line
+print(lineCNT)#16908
+lineCNT = 0
+with open("./data/dev.txt",'r',encoding='utf-8') as rf:
+    for line in rf:
+        if line != '':
+            lineCNT = lineCNT + 1
+            if lineCNT % 2 == 1:
+                dev_rev = dev_rev + line
+            else:            
+                dev_labes = dev_labes + line
+print(lineCNT)#2188
+lineCNT = 0
+with open("./data/test.txt",'r',encoding='utf-8') as rf:
+    for line in rf:
+        if line != '':
+            lineCNT = lineCNT + 1
+            if lineCNT % 2 == 1:
+                test_rev = test_rev + line
+            else:            
+                test_labes = test_labes + line
+print(lineCNT)#4400
+reviews = train_rev + dev_rev + test_rev
+
+from string import punctuation
+all_text = ''.join([c for c in reviews if c not in punctuation])
+#print(all_text[:1000])  str
+
+reviews = all_text.split('\n')
+all_text = ' '.join(reviews)
+words = all_text.split()
+reviews.pop()
+
+train_rev = ''.join([c for c in train_rev if c not in punctuation])
+train_rev = train_rev.split('\n')
+train_rev.pop()
+
+dev_rev = ''.join([c for c in dev_rev if c not in punctuation])
+dev_rev = dev_rev.split('\n')
+dev_rev.pop()
+
+test_rev = ''.join([c for c in test_rev if c not in punctuation])
+test_rev = test_rev.split('\n')
+test_rev.pop()
+'''
+reviews list 句子的合集
+all_text str 所有句子数据
+words   list 词语合集
+
+'''
+
+'''
+with open("./data/allText.txt",'w',encoding='utf-8') as wf:
+    wf.write(all_text)
+'''
+
+## 2 get dic for encoding the words
+from collections import Counter
+count = Counter(words)
+
+vocab = sorted(count,key=count.get,reverse=True)
+
+vocab_to_int = {word:i for i,word in enumerate(vocab,1)}
+
+print("len(vocab_to_int) = " + str(len(vocab_to_int)))
+
+
+## 3 encoding word and label train dev test 
+seq_len = 52
+from tensorflow.contrib.keras import preprocessing
+
+train_reviews_ints = []
+for each in train_rev:
+    train_reviews_ints.append([vocab_to_int[word] for word in each.split()])
+'''
+review_lens = Counter([len(x) for x in reviews_ints])
+print("Zero-length reviews: {}".format(review_lens[0]))
+print("Maximum review length: {}".format(max(review_lens)))
+'''
+features = np.zeros((len(train_reviews_ints),seq_len),dtype=int)
+features = preprocessing.sequence.pad_sequences(train_reviews_ints,52)
+
+labels = train_labes.split('\n')
+labels.pop()
+labels = np.array([int(each) for each in labels])
+
+np.save("./data/trainFea.npy",features)
+np.save("./data/trainLabel.npy",labels)
+
+dev_reviews_ints = []
+for each in dev_rev:
+    dev_reviews_ints.append([vocab_to_int[word] for word in each.split()])
+
+features = np.zeros((len(dev_reviews_ints),seq_len),dtype=int)
+features = preprocessing.sequence.pad_sequences(dev_reviews_ints,52)
+
+labels = dev_labes.split('\n')
+labels.pop()
+labels = np.array([int(each) for each in labels])
+
+np.save("./data/devFea.npy",features)
+np.save("./data/devLabel.npy",labels)
---
+++ wv.py
@@ -0,0 +1,90 @@
+import numpy as np
+import os
+import tensorflow as tf
+
+#len(vocab_to_int) = 21337
+word_num = 21337
+seq_len = 52
+lstm_hiden_size = 256
+lstm_hiden_layers = 1
+batch_size = 128
+learning_rate = 0.001
+
+train_fea = np.load("./data/trainFea.npy")
+train_label = np.load("./data/trainLabel.npy")
+dev_fea = np.load("./data/trainFea.npy")
+dev_label = np.load("./data/trainLabel.npy")
+
+tf.reset_default_graph()
+X = tf.placeholder(tf.int32,[None,seq_len],name='inputs')
+labels_ = tf.placeholder(tf.int32,[None,1],name='labels')
+keep_prob = tf.placeholder(tf.float32,name='keep_prob')
+
+embed_size = 300 
+
+embedding = tf.Variable(tf.random_uniform((word_num,embed_size),-1,1))
+embed = tf.nn.embedding_lookup(embedding,X)
+
+
+lstm = tf.contrib.rnn.BasicLSTMCell(lstm_hiden_size)
+
+drop = tf.contrib.rnn.DropoutWrapper(lstm,output_keep_prob=keep_prob)
+
+cell = tf.contrib.rnn.MultiRNNCell([drop]*lstm_hiden_layers)
+
+initial_state = cell.zero_state(batch_size,tf.float32)
+
+outputs,final_state = tf.nn.dynamic_rnn(cell=cell,inputs=embed,initial_state=initial_state)
+
+max_pool = tf.reduce_max(outputs,reduction_indices=[1])
+predictions = tf.contrib.layers.fully_connected(max_pool, 1, activation_fn=tf.sigmoid)
+with tf.name_scope('cost'):
+    cost = tf.losses.mean_squared_error(labels_, predictions)
+tf.summary.scalar('cost',cost)
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+with tf.name_scope('accuracy'):
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.cast(tf.round(predictions), tf.int32), labels_), tf.float32))
+tf.summary.scalar('accuracy',accuracy)
+def get_batches(x, y, batch_size):
+    n_batches = len(x)//batch_size
+    x, y = x[:n_batches*batch_size], y[:n_batches*batch_size]
+    for ii in range(0, len(x), batch_size):
+        yield x[ii:ii+batch_size], y[ii:ii+batch_size]
+
+merged = tf.summary.merge_all()
+direc = './graph/'
+train_writer = tf.summary.FileWriter(direc+'train',tf.get_default_graph())
+test_writer = tf.summary.FileWriter(direc+'test',tf.get_default_graph())
+
+epochs = 2
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    iteration = 1
+    for e in range(epochs):
+        
+        for ii, (x, y) in enumerate(get_batches(train_fea, train_label, batch_size), 1):
+            feed = {X: x,
+                    labels_: y[:,None],
+                    keep_prob:0.6}
+            loss, _, summary1 = sess.run([cost, optimizer, merged], feed_dict=feed)
+            
+            if iteration%5==0:
+                train_writer.add_summary(summary1,iteration)
+                print("Epoch: {}/{}".format(e+1, epochs),
+                      "Iteration: {}".format(iteration),
+                      "Train loss: {:.3f}".format(loss))
+
+            if iteration%10==0:
+                val_acc = []
+                for x, y in get_batches(dev_fea, dev_label, batch_size):
+                    feed = {X: x,
+                            labels_: y[:,None],
+                            keep_prob:1.0}
+                    batch_acc, summary2 = sess.run([accuracy, merged], feed_dict=feed)
+                    val_acc.append(batch_acc)
+                test_writer.add_summary(summary2,iteration)
+                print("Val acc: {:.3f}".format(np.mean(val_acc)))
+            iteration +=1
+    saver.save(sess, "./model/sentiment.ckpt")
